5b5218609c111750a302ecdac3c4bd3af8299f12
YunJuBanXia <yunjubanxia@gmail.com>
2026-04-19 00:16:28 +0800

    将连接池传入 Extension

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use axum::{Router};
-use blog::user;
+use axum::{Extension, Router};
+use blog::{db::init_db, user};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .nest("api/", user::urls::router());
+        .nest("api/", user::urls::router())
+        .layer(Extension(init_db().await.expect("Failed to initialize the database")));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 
 }
