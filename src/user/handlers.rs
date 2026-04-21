e08194b775b2454632e621982d58abe1f52a0439
YunJuBanXia <yunjubanxia@gmail.com>
2026-04-22 00:28:11 +0800

    格式化 注释

--- src/user/handlers.rs
+++ src/user/handlers.rs
@@ -6,7 +6,7 @@ use crate::user::models::User;
 pub async fn get_users(Extension(pool): Extension<PgPool>) -> impl IntoResponse {
     // 查询所有用户
     // 应尽量不使用这一函数, 而应该使用下面的 get_users_paged 来分页查询用户, 以避免一次性加载过多数据
-    // 且这一函数会暴露密码哈希
+    // 且这一函数会暴露密码哈希, 因为 Json(users) 不会调用 User 的 IntoResponse 实现, 而是直接序列化 User 结构体, 导致密码哈希被包含在响应中
     let users: Vec<User> = sqlx::query_as!(
         User,
         "SELECT id, username, email, password_hash, created_at as \"created_at!: chrono::DateTime<chrono::Utc>\" FROM users"
@@ -17,6 +17,7 @@ pub async fn get_users(Extension(pool): Extension<PgPool>) -> impl IntoResponse 
     
     Json(users)
 }
+
 
 pub async fn get_user_by_id(Path(id): Path<i32>, Extension(pool): Extension<PgPool>) -> impl IntoResponse {
     let user = sqlx::query_as!(
