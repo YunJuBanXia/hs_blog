72c2774cfa9a145a8fb0736549011250783cfa98
YunJuBanXia <yunjubanxia@gmail.com>
2026-04-22 00:25:52 +0800

    实现 get_users handler

--- src/user/handlers.rs
+++ src/user/handlers.rs
@@ -1,12 +1,24 @@
-use axum::{Extension, extract::Path, response::{IntoResponse, Response}};
+use axum::{Extension, Json, extract::Path, response::{IntoResponse, Response}};
 use sqlx::PgPool;
 use crate::user::models::User;
 
-pub async fn get_users() -> String {
-    todo!()
+
+pub async fn get_users(Extension(pool): Extension<PgPool>) -> impl IntoResponse {
+    // 查询所有用户
+    // 应尽量不使用这一函数, 而应该使用下面的 get_users_paged 来分页查询用户, 以避免一次性加载过多数据
+    // 且这一函数会暴露密码哈希
+    let users: Vec<User> = sqlx::query_as!(
+        User,
+        "SELECT id, username, email, password_hash, created_at as \"created_at!: chrono::DateTime<chrono::Utc>\" FROM users"
+    )
+        .fetch_all(&pool)
+        .await
+        .expect("Failed to fetch users");
+    
+    Json(users)
 }
 
-pub async fn get_user_by_id(Path(id): Path<i32>, Extension(pool): Extension<PgPool>) -> Response {
+pub async fn get_user_by_id(Path(id): Path<i32>, Extension(pool): Extension<PgPool>) -> impl IntoResponse {
     let user = sqlx::query_as!(
         User,
         "SELECT id, username, email, password_hash, created_at as \"created_at!: chrono::DateTime<chrono::Utc>\" FROM users WHERE id = $1",
@@ -16,5 +28,5 @@ pub async fn get_user_by_id(Path(id): Path<i32>, Extension(pool): Extension<PgPo
         .await
         .expect("Failed to fetch user by ID");
     
-    user.into_response()
+    user
 }
