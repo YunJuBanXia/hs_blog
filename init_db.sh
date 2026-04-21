055a6e432fa524cffe74d6cd5246110279aa8c6e
YunJuBanXia <yunjubanxia@gmail.com>
2026-04-21 23:23:46 +0800

    修复无法在正确的数据库下给予数据库用户权限的问题

--- init_db.sh
+++ init_db.sh
@@ -2,8 +2,8 @@
 set -e
 
 DB_NAME="hs_blog"
-DB_USER="root"
-DB_PASSWORD="rootroot"
+DB_USER="hs_user"
+DB_PASSWORD="0d000721"
 
 # Check whether the database already exists
 DB_EXISTS=$(sudo -u postgres psql -tAc "SELECT 1 FROM pg_database WHERE datname='${DB_NAME}'")
@@ -21,12 +21,16 @@ USER_EXISTS=$(sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='
 if [ "$USER_EXISTS" = "1" ]; then
 	echo "User '$DB_USER' already exists. Skipping user creation."
 else
+	# Create the user with the specified password
 	sudo -u postgres psql -c "CREATE USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASSWORD';"
 	echo "User '$DB_USER' created."
 fi
 
 # Grant privileges to the user on the database
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
+sudo -u postgres psql -U postgres -d $DB_NAME -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"  # Grant all privileges on the database
+sudo -u postgres psql -U postgres -d $DB_NAME -c "GRANT CREATE ON SCHEMA public TO $DB_USER;"  # Grant CREATE privilege on the public schema to allow creating tables
+sudo -u postgres psql -U postgres -d $DB_NAME -c "GRANT USAGE ON SCHEMA public TO $DB_USER;"  # Grant USAGE privilege on the public schema to allow using it
+sudo -u postgres psql -U postgres -d $DB_NAME -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $DB_USER;"  # Grant all privileges on existing tables in the public schema
 
 echo "Privileges granted on database '$DB_NAME' to user '$DB_USER'."
 
