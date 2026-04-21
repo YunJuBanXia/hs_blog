582782cc952d3aa4274ab7904312d63aad2f727e
YunJuBanXia <yunjubanxia@gmail.com>
2026-04-22 00:06:06 +0800

    为 User 实现 IntoRepsonse 特征并修复 Rust 字段与数据库字段不统一的问题

--- src/user/models.rs
+++ src/user/models.rs
@@ -1,32 +1,49 @@
+use axum::response::{IntoResponse, Response};
 use serde::{Serialize, Deserialize};
 use crate::user::pwd::Password;
 use chrono::{DateTime, Utc};
 
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, sqlx::FromRow)]
 pub struct User {
-    id: u32,
-    name: String,
-    email: String,
-    password: Password,
-    created_at: DateTime<Utc>,
+    pub id: i32,
+    pub username: String,
+    pub email: String,
+    pub password_hash: String,
+    pub created_at: DateTime<Utc>,
 }
 
 
 impl User {
-    pub fn new(id: u32, name: String, email: String, raw_password: String) -> Self {
-        let password = Password::new(raw_password);
+    pub fn new(id: i32, username: String, email: String, raw_password: String) -> Self {
+        let pwd = Password::new(raw_password);
+        let password_hash = pwd.0.clone();
         let created_at = Utc::now();
-        Self { id, name, email, password, created_at }
+        Self { id, username, email, password_hash, created_at }
     }
 
     
     pub fn set_password(&mut self, raw_password: String) {
-        self.password = Password::new(raw_password);
+        let pwd = Password::new(raw_password);
+        self.password_hash = pwd.0.clone();
     }
 
 
     pub fn check_password(&self, raw_pwd: String) -> bool {
-        self.password.verify(raw_pwd)
+        let pwd_obj = Password(self.password_hash.clone());
+        pwd_obj.verify(raw_pwd)
+    }
+}
+
+
+impl IntoResponse for User {
+    fn into_response(self) -> Response {
+        let user_info = serde_json::json!({
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "created_at": self.created_at,
+        });
+        axum::Json(user_info).into_response()
     }
 }
