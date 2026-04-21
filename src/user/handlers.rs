43ce49ccbd6fe0e65aa366ff46a8208a85160143
YunJuBanXia <yunjubanxia@gmail.com>
2026-04-22 00:04:21 +0800

    实现 get_user_by_id handler

--- src/user/handlers.rs
+++ src/user/handlers.rs
@@ -1,10 +1,21 @@
-
-
+use axum::{Extension, extract::Path, response::{IntoResponse, Response}};
+use sqlx::PgPool;
+use crate::user::models::User;
 
 pub async fn get_users() -> String {
     todo!()
 }
 
-pub async fn get_user_by_id() -> String {
-    todo!()
+pub async fn get_user_by_id(Path(id): Path<i32>, Extension(pool): Extension<PgPool>) -> Response {
+    dotenvy::dotenv().ok();
+    let user = sqlx::query_as!(
+        User,
+        "SELECT id, username, email, password_hash, created_at as \"created_at!: chrono::DateTime<chrono::Utc>\" FROM users WHERE id = $1",
+        id
+    )
+        .fetch_one(&pool)
+        .await
+        .expect("Failed to fetch user by ID");
+    
+    user.into_response()
 }
