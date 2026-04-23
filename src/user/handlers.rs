1c34ccaecc29a96b8822702833c9df1953b30aa0
YunJuBanXia <yunjubanxia@gmail.com>
2026-04-23 20:12:20 +0800

    改进 get_users 错误处理

--- src/user/handlers.rs
+++ src/user/handlers.rs
@@ -1,19 +1,21 @@
-use axum::{Extension, Json, extract::Path, response::IntoResponse};
+use axum::{Extension, Json, extract::Path, http::StatusCode, response::IntoResponse};
 use sqlx::PgPool;
 use crate::user::models::{UserResponse};
 
 pub async fn get_users(Extension(pool): Extension<PgPool>) -> impl IntoResponse {
     // 查询所有用户
     // 应尽量不使用这一函数, 而应该使用下面的 get_users_paged 来分页查询用户, 以避免一次性加载过多数据
-    let users: Vec<UserResponse> = sqlx::query_as!(
+    let result: Result<Vec<UserResponse>, sqlx::Error> = sqlx::query_as!(
         UserResponse,
         "SELECT id, username, email, created_at as \"created_at!: chrono::DateTime<chrono::Utc>\" FROM users"
     )
         .fetch_all(&pool)
-        .await
-        .expect("Failed to fetch users");
+        .await;
     
-    Json(users)
+    match result {
+        Ok(users) => (StatusCode::OK, Json(users)).into_response(),
+        Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Failed to fetch users").into_response()
+    }
 }
 
 
