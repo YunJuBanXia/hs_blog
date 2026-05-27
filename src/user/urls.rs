a905e574a25e36e0886f0b4933df4a3f391ab6ce
YunJuBanXia <yunjubanxia@gmail.com>
2026-05-28 04:20:59 +0800

    注册路由

--- src/user/urls.rs
+++ src/user/urls.rs
@@ -7,4 +7,6 @@ pub fn router() -> Router<Pool<Postgres>> {
         .route("users", get(handlers::list_users))
         .route("users/{id}", get(handlers::get_user))
         .route("users/register", post(handlers::register))
+        .route("users/login", post(handlers::login))
+        .route("users/refresh_token", post(handlers::refresh_token))
 }
